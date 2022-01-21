93d60661ff8491bc45a4b3fdb840b067ec280b9a
Bohdan Nedashkivskyi <bohdan.nedashkivskyi.1@gmail.com>
2022-01-21 12:50:27 +0200

    add setOnClickListener for icons

--- app/src/main/java/com/nedash/com/smartwatch/notifier/app/ui/fragment/apps_settings/AppsSettingsFragment.kt
+++ app/src/main/java/com/nedash/com/smartwatch/notifier/app/ui/fragment/apps_settings/AppsSettingsFragment.kt
@@ -7,14 +7,9 @@ import android.view.ViewGroup
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.nedash.com.smartwatch.notifier.app.R
-import com.nedash.com.smartwatch.notifier.app.databinding.AppsListItemBinding
 import com.nedash.com.smartwatch.notifier.app.databinding.FragmentAppsSettingsBinding
 import com.nedash.com.smartwatch.notifier.app.utils.Utils.gone
-import com.nedash.com.smartwatch.notifier.app.utils.Utils.navigateWithSlideAnimation
 import com.nedash.com.smartwatch.notifier.app.utils.Utils.visible
 import dagger.hilt.android.AndroidEntryPoint
 
